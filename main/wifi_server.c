335a44def6513ef1700f8b22175fb857de913069
Brandon Ros <brandonros1@gmail.com>
2021-08-17 21:15:49 -0400

    wifi works

--- main/wifi_server.c
+++ main/wifi_server.c
@@ -9,12 +9,23 @@ const char *WIFI_SERVER_TAG = "wifi_server";
 void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*)event_data;
         ESP_LOGI(WIFI_SERVER_TAG, "station "MACSTR" join, AID=%d", MAC2STR(event->mac), event->aid);
     } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*)event_data;
         ESP_LOGI(WIFI_SERVER_TAG, "station "MACSTR" leave, AID=%d", MAC2STR(event->mac), event->aid);
     }
+}
+
+void dhcp_server_setup()
+{
+  ESP_ERROR_CHECK(tcpip_adapter_dhcps_stop(TCPIP_ADAPTER_IF_AP));
+  tcpip_adapter_ip_info_t ip_info;
+  IP4_ADDR(&ip_info.ip,192,168,4,1);
+  IP4_ADDR(&ip_info.gw,192,168,4,1);
+  IP4_ADDR(&ip_info.netmask,255,255,255,0);
+  ESP_ERROR_CHECK(tcpip_adapter_set_ip_info(TCPIP_ADAPTER_IF_AP, &ip_info));
+  ESP_ERROR_CHECK(tcpip_adapter_dhcps_start(TCPIP_ADAPTER_IF_AP));
 }
 
 void wifi_server_setup()
@@ -41,6 +52,7 @@ void wifi_server_setup()
   };
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
   ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+  dhcp_server_setup();
   ESP_ERROR_CHECK(esp_wifi_start());
   ESP_LOGI(WIFI_SERVER_TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d", CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD, CONFIG_ESP_WIFI_CHANNEL);
 }
