c9c656c803d54307e85fe218e954b3bda6794a4d
Brandon Ros <brandonros1@gmail.com>
2021-08-22 19:22:10 -0400

    100ms isn't enough to receive largeish messages

--- main/isotp_config.h
+++ main/isotp_config.h
@@ -2,9 +2,9 @@
 #define __ISOTP_CONFIG__
 
 /* Max number of messages the receiver can receive at one time, this value 
- * is affectied by can driver queue length
+ * is affectied by CAN driver queue length
  */
-#define ISO_TP_DEFAULT_BLOCK_SIZE   8
+#define ISO_TP_DEFAULT_BLOCK_SIZE   0x20
 
 /* The STmin parameter value specifies the minimum time gap allowed between 
  * the transmission of consecutive frame network protocol data units
@@ -19,11 +19,11 @@
 /* Private: The default timeout to use when waiting for a response during a
  * multi-frame send or receive.
  */
-#define ISO_TP_DEFAULT_RESPONSE_TIMEOUT 100
+#define ISO_TP_DEFAULT_RESPONSE_TIMEOUT 1000
 
 /* Private: Determines if by default, padding is added to ISO-TP message frames.
  */
-#define ISO_TP_FRAME_PADDING
+#define ISO_TP_FRAME_PADDING       1
 
 #endif
 
