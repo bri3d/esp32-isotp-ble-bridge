730324d5c7962b28f3130383c248a162d7c45fa7
Brandon Ros <brandonros1@gmail.com>
2021-08-22 18:18:44 -0400

    const arbitration identifiers

--- main/arbitration_identifiers.c
+++ main/arbitration_identifiers.c
@@ -1,10 +1,10 @@
 #include <stdint.h>
 #include "arbitration_identifiers.h"
 
-uint32_t read_uint32_be(uint8_t *data) {
+uint32_t read_uint32_be(const uint8_t *data) {
   return data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
 }
 
-uint32_t read_uint32_le(uint8_t *data) {
+uint32_t read_uint32_le(const uint8_t *data) {
   return data[3] | (data[2] << 8) | (data[1] << 16) | (data[0] << 24);
 }
--- main/arbitration_identifiers.h
+++ main/arbitration_identifiers.h
@@ -1,4 +1,4 @@
 #include <stdint.h>
 
-uint32_t read_uint32_be(uint8_t *data);
-uint32_t read_uint32_le(uint8_t *data);
+uint32_t read_uint32_be(const uint8_t *data);
+uint32_t read_uint32_le(const uint8_t *data);
