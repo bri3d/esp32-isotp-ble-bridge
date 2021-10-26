a5822923e289d669c3c81299396e4e4678a55bd1
Switchleg1 <89221799+Switchleg1@users.noreply.github.com>
2021-10-26 06:49:56 -0600

    Delete updater_com3.bat

--- release/updater_com3.bat
+++
@@ -1,7 +0,0 @@
-esptool.exe --chip esp32 --port 3 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x1000 bootloader.bin 0x10000 isotp_ble_bridge.bin 0x8000 partition-table.bin
-esptool.exe --chip esp32 --port com3 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x1000 bootloader.bin 0x10000 isotp_ble_bridge.bin 0x8000 partition-table.bin
-@pause  
- 
-
-
-
